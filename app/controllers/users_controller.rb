b62e58e58f1cfb0d854958dcb1a02b2e3f731ec0
Tatsuya Natori <tatsuya.n.22128@gmail.com>
2020-02-07 17:24:21 +0000

    authenticate function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -32,11 +35,32 @@ class UsersController < ApplicationController
       flash[:danger] = '更新に失敗しました。'
       render :edit
     end 
-  end 
+  end
   
   private
-  
+    
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
+    
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end 
+    
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end 
+    end 
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end 
 end
