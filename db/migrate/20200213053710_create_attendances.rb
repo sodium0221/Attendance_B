ca622d07178a9a79f1cd3e62b006e8034cd0657a
Tatsuya Natori <tatsuya.n.22128@gmail.com>
2020-02-13 05:42:28 +0000

    add_attendance_model

---
+++ db/migrate/20200213053710_create_attendances.rb
@@ -0,0 +1,14 @@
+class CreateAttendances < ActiveRecord::Migration[5.1]
+  def change
+    create_table :attendances do |t|
+      t.date :worked_on
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.string :note
+      t.integer :user_id
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
