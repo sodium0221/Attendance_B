bece5242bf3cc54bedd0daf599927bc0b8c2d718
Tatsuya Natori <tatsuya.n.22128@gmail.com>
2020-02-17 09:06:07 +0000

    Finish add-user-attendance

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -3,15 +3,20 @@ module AttendancesHelper
   def attendance_state(attendance)
     # 受け取った Attendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
-      return '出社' if attendance.started_at.nil?
+      return '出勤' if attendance.started_at.nil?
     end 
     false
   end 
   
   def quitting_state(attendance)
     if Date.current == attendance.worked_on
-      return '退社' if attendance.started_at.present? && attendance.finished_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end 
     false
+  end 
+  
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
   end 
 end
