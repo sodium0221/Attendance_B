9cf1b3d685b4aff3c3d4c49be42a148105b3d19d
Tatsuya Natori <tatsuya.n.22128@gmail.com>
2020-06-25 11:12:22 +0000

    仕様書No.9

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,8 @@ class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
-  before_action :set_one_month, only: :edit_one_month
+  before_action :set_one_month, only: [:edit_one_month, :update_one_month]
+
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
@@ -22,7 +23,7 @@ class AttendancesController < ApplicationController
       else
         flash[:danger] = UPDATE_ERROR_MSG
       end 
-    end 
+    end
     redirect_to @user
   end
   
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,18 +4,30 @@ class Attendance < ApplicationRecord
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
 
-  # 出勤時間が存在しない場合、退勤時間は無効
-  validate :finished_at_is_invalid_without_a_started_at
+  # 当日以外の出勤・退勤時間のどちらかのみの更新は無効
+  validate :only_started_at_or_finished_at_present_if_invalid
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
-  def finished_at_is_invalid_without_a_started_at
-    errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
-  end
-  
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end 
   end 
+  
+  def only_started_at_or_finished_at_present_if_invalid
+    if worked_on != Date.current
+      if started_at.blank? && finished_at.present?
+        errors.add(:started_at, "が必要です")
+      elsif finished_at.blank? && started_at.present?
+        errors.add(:finished_at, "が必要です")
+      end 
+    else 
+      if started_at.blank? && finished_at.present?
+        errors.add(:started_at, "が必要です")
+      end 
+    end 
+  end 
+      
+
 end
